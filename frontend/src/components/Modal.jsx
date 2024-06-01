719c389ee9539068be11f40965d61572c79edee9
Mark <mrkaxelus@gmail.com>
2024-06-01 11:15:24 -0700

    update

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -5,9 +5,20 @@ const Modal = ( { isOpen, onClose }) => {
   if (!isOpen) return null;
   
   return (
-    <div className="">
-      <div className='text-lg'>
-        <button onClick={onClose}>X</button>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className='bg-white rounded-lg p-4 '> 
+
+        <div>
+          <button onClick={onClose}>X</button>
+        </div>
+    {/*
+    TITLE:
+          IMAGE:
+          RATINGS
+    */}
+        <div>
+          
+        </div>
       </div>  
     </div>
   )
