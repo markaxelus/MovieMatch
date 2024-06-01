168721c9bb52ecf430dc9da66b6575e4d5085dfe
oliviachoiii <oliviachoi612@gmail.com>
2024-06-01 12:04:25 -0700

    Updated repository structure

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/markaxelus/Documents/MovieMatch/backend/venv"
+set -gx VIRTUAL_ENV "/Users/arc2106/Desktop/Career/MovieMatch/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
